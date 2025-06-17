9b22ff411c96701c46878877306b1c0d064db3fa
steve <chaunguyen4297@gmail.com>
2025-06-17 23:08:23 +0700

    chỉnh lại tên nút về page

--- components/forms/EditProductForm.tsx
+++ components/forms/EditProductForm.tsx
@@ -140,7 +140,7 @@ const EditProductPage = () => {
           href="/dashboard/products"
           className="inline-block bg-gray-200 hover:bg-gray-300 text-gray-800 px-4 py-2 rounded-md transition"
         >
-          ← Quay lại danh sách sản phẩm
+          ← Return
         </Link>
       </div>
 
