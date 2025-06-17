e6123cb1606fb3a786bf5342a03b85707ed8793c
nguyentranbaotuan24 <nguyentranbaotuan24@gmail.com>
2025-06-17 20:03:48 +0700

    Exit trả về Sign in

--- components/dashboard/header/DashboardHeader.tsx
+++ components/dashboard/header/DashboardHeader.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Logo from "@/components/logo/Logo";
 import { ThemeToggle } from "@/components/theme/ThemeToggle";
 import { Button } from "@/components/ui/button";
@@ -5,8 +6,19 @@ import { Bell, LogOut } from "lucide-react";
 import React from "react";
 import Notification from "../notificaton/Notification";
 import DashboardMobileHeader from "./DashboardMobileHeader";
+import { useRouter } from "next/navigation";
+import { useAuthStore } from "@/store/authStore";
 
 const DashboardHeader = () => {
+
+const router = useRouter();
+const logout = useAuthStore((state) => state.logout);
+
+const handleLogout = () => {
+  logout();
+  router.push("/sign-in");
+};
+
   return (
     <header className="bg-white dark:bg-gray-900 shadow sticky top-0 left-0 right-0 z-50">
       <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex items-center justify-between">
@@ -15,9 +27,10 @@ const DashboardHeader = () => {
           <ThemeToggle />
           <Notification />
           <Button
-            size={"sm"}
-            variant={"destructive"}
+            size="sm"
+            variant="destructive"
             className="flex items-center gap-2"
+            onClick={handleLogout}
           >
             <LogOut /> Exit
           </Button>
